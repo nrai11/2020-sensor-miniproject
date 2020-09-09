a7fe41b832b241b4a40556faa6de5d38dd7d65e3
nrai11 <nrai@bu.edu>
2020-09-09 11:33:08 -0400

    fixed client.py

--- src/sp_iotsim/client.py
+++ src/sp_iotsim/client.py
@@ -22,8 +22,8 @@ async def main(port: int, addr: str, max_packets: int, log_file: Path):
 
     if log_file:
         log_file = Path(log_file).expanduser()
+        file = log_file.open("a")
     uri = f"ws://{addr}:{port}"
-    file = log_file.open("a")
     async with websockets.connect(uri) as websocket:
         qb = await websocket.recv()
         if isinstance(qb, bytes):
@@ -37,5 +37,7 @@ async def main(port: int, addr: str, max_packets: int, log_file: Path):
                 pass
                 # print(f"{i} total messages received")
             print(data)
-            file.write(data + "\n")
-            file.flush()
+            if log_file:
+                file.write(data + "\n")
+                file.flush()
+        file.close()
